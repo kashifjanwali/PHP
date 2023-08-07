b955e24e2b3dd6935fc2f186779bf9ef5090620b
Kashif Hussain <kh9732494@gmail.com>
2023-08-07 23:23:37 +0500

    Variables | $var_name

---
+++ variables/var.php
@@ -0,0 +1,10 @@
+<?php
+$f1 = "Kashif";
+$f2 = "Hussain";
+$f3 = "Ali";
+$f4 = "Muhammad";
+$f5 = "Karamat";
+
+print $f1 . "<br>" . $f2 . "<br>" . $f3 . "<br>" . $f4 . "<br>" . $f5 . "<br> <br>";
+echo $f1 . "<br>" . $f2 . "<br>" . $f3 . "<br>" . $f4 . "<br>" . $f5 . "<br> <br>";
+?>
